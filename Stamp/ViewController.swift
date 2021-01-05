029c391a827d2f5dafb43c328df8ea963b36e0bc
西山朋佳 <hono.nish6555@gmail.com>
2021-01-05 20:03:21 +0900

    Initial Commit

---
+++ Stamp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stamp
+//
+//  Created by Honoka Nishiyama on 2021/01/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
